9c7d0cfb66cb95598e0985b6e80fb279bde1991f
Taylor Turner <taylorfturner@gmail.com>
2017-04-03 12:57:26 -0400

    production.rb file update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '159.203.80.148', user: 'deploy', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
