68ec37c5fbda9483a34bc612a3e337a5936f9ba1
Taylor Turner <taylorfturner@gmail.com>
2017-04-03 15:06:51 -0400

    shange to repo link

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "deploy_test"
-set :repo_url, "git@github.com:taylorturneriv/deploy_test.git"
+set :repo_url, "ssh://git@github.com:taylorturneriv/deploy_test.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
