6f1ee5f4558656447f070cf4874709c8d257992a
Taylor Turner <taylorfturner@gmail.com>
2017-04-06 10:53:48 -0400

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :investors
   resources :users
 
-  root "users#index"
+  root "investors#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
