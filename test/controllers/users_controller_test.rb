dfeab08705959c03c30df607bfadcbb2cded2091
Taylor Turner <taylorfturner@gmail.com>
2017-04-06 11:07:08 -0400

    generate scaffold users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email } }
+      post users_url, params: { user: { email: @user.email, name: @user.name, password_digest: @user.password_digest } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email } }
+    patch user_url(@user), params: { user: { email: @user.email, name: @user.name, password_digest: @user.password_digest } }
     assert_redirected_to user_url(@user)
   end
 
