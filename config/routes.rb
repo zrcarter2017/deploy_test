c468c794dab07e218873e890418c70b563a3e713
Taylor Turner <taylorfturner@gmail.com>
2017-04-06 11:10:36 -0400

    fix root route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :notes
   resources :investors
 
-  root "investors#index"
+  root "notes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
