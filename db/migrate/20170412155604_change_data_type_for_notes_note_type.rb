b5ecc317bb2a4c1c97eb158d6bf8556f3ece801d
Taylor Turner <taylorfturner@gmail.com>
2017-04-12 12:01:31 -0400

    fix data type migration syntax

--- db/migrate/20170412155604_change_data_type_for_notes_note_type.rb
+++ db/migrate/20170412155604_change_data_type_for_notes_note_type.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeForNotesNoteType < ActiveRecord::Migration[5.0]
   def change
-  	change_column :notes :note_type :double
+  	change_column :notes, :note_type, :double
   end
 end
