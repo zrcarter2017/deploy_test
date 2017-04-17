67dab6ad8f75d4e258d4c052b93ad9ba7b3366f3
Taylor Turner <taylorfturner@gmail.com>
2017-04-17 13:41:33 -0400

    concat of note_id on notes table

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,8 +2,8 @@ class Note < ApplicationRecord
 
   belongs_to :investor
 
-	def note_id
-	  "#{self.address_1}, #{self.address_2}, #{self.city}"
+	def create_note_id
+	  "#{self.id},#{self.investor_id}_#{self.note_type}"
 	end
 
 end
