002e8d4de4cf674f60cc301466e2d4dab22ad9c5
Taylor Turner <taylorfturner@gmail.com>
2017-04-17 13:18:31 -0400

    update note model for concat field

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,11 +1,9 @@
 class Note < ApplicationRecord
-  
-  # before :save, :set_note_id
 
   belongs_to :investor
 
-  # def set_note_id
-  # 	note_id = "#{investor_id}_#{note_type}"
-  # end
+	def note_id
+	  "#{self.address_1}, #{self.address_2}, #{self.city}"
+	end
 
 end
