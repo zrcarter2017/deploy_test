8ce250e59e51e60b2fb617f08ca4f71b3bfd2ed1
Taylor Turner <taylorfturner@gmail.com>
2017-04-17 13:06:33 -0400

    remove set_note_id in model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,11 +1,11 @@
 class Note < ApplicationRecord
   
-  before :save, :set_note_id
+  # before :save, :set_note_id
 
   belongs_to :investor
 
-  def set_note_id
-  	note_id = "#{investor_id}_#{note_type}"
-  end
+  # def set_note_id
+  # 	note_id = "#{investor_id}_#{note_type}"
+  # end
 
 end
