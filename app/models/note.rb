dfce16cb4b12ff7852b404bac7f9949ed6ca6005
Taylor Turner <taylorfturner@gmail.com>
2017-04-19 17:39:25 -0400

    fix before_save

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,7 @@ class Note < ApplicationRecord
 
   belongs_to :investor
 	
-  before_save :concatenate_details
+  before_save :note_id
 
   def note_id
 	self.note_id = (params[:investor_id].to_s).concat('_').concat(params[:invest_date].to_s).concat('_').concat(params[:note_type].to_s)
