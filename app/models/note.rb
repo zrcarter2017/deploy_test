95df4b9dc96be2ecedc300492f28e72e58c26e00
Taylor Turner <taylorfturner@gmail.com>
2017-04-20 12:18:55 -0400

    clean up model note.rb from concat

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,10 +2,10 @@ class Note < ApplicationRecord
 
   belongs_to :investor
 	
-  before_save :note_id
+  # before_save :note_id
 
-  def note_id
-  	[investor_id, invest_date, note_type].join('_')
-  end
+  # def note_id
+  # 	[investor_id, invest_date, note_type].join('_')
+  # end
   
 end
