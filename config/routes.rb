38efba3978f681dc372bd856710216c0d4330319
Taylor Turner <taylorfturner@gmail.com>
2017-04-25 13:22:19 -0400

    add resource notify

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :notetypes
   resources :investors
   resources :notes
-  resources :criterias
+  resources :notify
 
   root "investors#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
