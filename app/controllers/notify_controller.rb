b7ec6415b10d53705bde1aea2c3e7188c4736fc2
Taylor Turner <taylorfturner@gmail.com>
2017-04-25 13:41:56 -0400

    fix to notify controllerg

--- app/controllers/notify_controller.rb
+++ app/controllers/notify_controller.rb
@@ -2,8 +2,8 @@
     
     def index
       @notify_investors = Report::Notify
-      # respond_to do |format|
-      #   format.html {}
+      respond_to do |format|
+        format.html {}
       end
     end
 
