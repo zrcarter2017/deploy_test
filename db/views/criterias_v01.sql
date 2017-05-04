3039025ef18e90e4c7821d964363e26962b7f507
Taylor Turner <taylorfturner@gmail.com>
2017-05-04 12:29:07 -0400

    fix to mysql view to ruby view

--- db/views/criterias_v01.sql
+++ db/views/criterias_v01.sql
@@ -1,13 +1,15 @@
 SELECT 
+	1 as `id`,
 	entity_name, 
 	last_name, 
 	first_name, 
 	invest_date, 
 	email, 
 	note_type, 
-	dollar_invest, 
-	DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) AS `NOTIFY_DATE`
+	dollar_invest,
+	DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) AS `NOTIFY_DATE`,
+	DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY) AS `MATURITY_DATE`
 FROM notes 
 LEFT JOIN investors on `notes`.`investor_id` = `investors`.`id` 
-where DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) > DATE_SUB(DATE_SUB(DATE_ADD(date(now()), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY), INTERVAL 30 DAY) and DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) < DATE_ADD(DATE_SUB(DATE_ADD(date(now()), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY), INTERVAL 30 DAY)
+where DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) > DATE_SUB(DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY), INTERVAL 30 DAY) and date(now()) > DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY) 
 ORDER BY DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12) * 30 DAY), INTERVAL 30 DAY) ASC;
