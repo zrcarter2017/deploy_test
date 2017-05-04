bc3bbff2ea582881a6990ae095117be5f047c93f
Taylor Turner <taylorfturner@gmail.com>
2017-05-04 10:44:57 -0400

    fix to sql view

--- db/views/criterias_v01.sql
+++ db/views/criterias_v01.sql
@@ -1,1 +1,13 @@
-select entity_name, last_name, first_name, invest_date, email, note_type, dollar_invest, DATE_SUB(DATE_ADD(date(now()), INTERVAL (note_type * 12)*30 DAY),INTERVAL 30 DAY) AS `NOTIFY_DATE` from notes LEFT JOIN investors on `notes`.`investor_id` = `investors`.`id` ORDER BY DATE_SUB(DATE_ADD(date(now()), INTERVAL (note_type * 12)*30 DAY),INTERVAL 30 DAY) ASC;
+SELECT 
+	entity_name, 
+	last_name, 
+	first_name, 
+	invest_date, 
+	email, 
+	note_type, 
+	dollar_invest, 
+	DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) AS `NOTIFY_DATE`
+FROM notes 
+LEFT JOIN investors on `notes`.`investor_id` = `investors`.`id` 
+where DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) > DATE_SUB(DATE_SUB(DATE_ADD(date(now()), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY), INTERVAL 30 DAY) and DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) < DATE_ADD(DATE_SUB(DATE_ADD(date(now()), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY), INTERVAL 30 DAY)
+ORDER BY DATE_SUB(DATE_ADD(date(now()), INTERVAL (note_type * 12) * 30 DAY), INTERVAL 30 DAY) ASC;
