2b2cd379877d9a71c3099a364db0eb484a703015
Taylor Turner <taylorfturner@gmail.com>
2017-06-28 09:55:44 -0400

    new graph geo

---
+++ db/views/viz_view.sql
@@ -0,0 +1,16 @@
+SELECT 
+	1 as `id`,
+	entity_name, 
+	last_name, 
+	first_name, 
+	invest_date, 
+	email, 
+	broker, 
+	rep,
+	note_type, 
+	dollar_invest,
+	DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY), INTERVAL 30 DAY) AS `NOTIFY_DATE`,
+	DATE_ADD(date(invest_date), INTERVAL (note_type * 12)*30 DAY) AS `MATURITY_DATE`
+FROM notes 
+LEFT JOIN investors on `notes`.`investor_id` = `investors`.`id` 
+ORDER BY DATE_SUB(DATE_ADD(date(invest_date), INTERVAL (note_type * 12) * 30 DAY), INTERVAL 30 DAY) ASC;
